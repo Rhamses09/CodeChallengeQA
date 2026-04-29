8140be531a064f78b6dcbea1eb46a0a950fb8dfb
rhamses <rhamses09.rot@gmail.com>
2026-04-29 05:04:30 -0700

    Rhamses: Controller agian

--- src/main/java/mx/edu/cetys/software_quality_lab/users/UserService.java
+++ src/main/java/mx/edu/cetys/software_quality_lab/users/UserService.java
@@ -2,6 +2,8 @@ package mx.edu.cetys.software_quality_lab.users;
 
 import mx.edu.cetys.software_quality_lab.users.exceptions.InvalidUserDataException;
 import mx.edu.cetys.software_quality_lab.validators.EmailValidatorService;
+import mx.edu.cetys.software_quality_lab.validators.UserNotFoundException;
+import mx.edu.cetys.software_quality_lab.validators.DuplicateUsernameException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -44,32 +46,32 @@ public class UserService {
 
         // checar si el firstName esta entre 2 a 50 caracteres y con restricciones validas
         if (request.firstName() == null || request.firstName().isBlank() || request.firstName().length() > 50 || request.firstName().length() < 2 || !request.firstName().matches("^[a-zA-Záéíóúñ]")){
-            throw new InvalidUserDataException("El primer nombre no es valido")
+            throw new InvalidUserDataException("El primer nombre no es valido");
         }
         // Lo mismo pero con el lastName
         if (request.lastName() == null || request.lastName().isBlank() || request.lastName().length() > 50 || request.lastName().length() < 2 || !request.lastName().matches("^[a-zA-Záéíóúñ]")){
-            throw new InvalidUserDataException("El ultimo nombre no es valido")
+            throw new InvalidUserDataException("El ultimo nombre no es valido");
         }
 
         // checar si la edad es mayor a 12 o igual o menor a 120
         if (request.age() < 12 || request.age() > 120){
-            throw new InvalidUserDataException("La edad valida es solo entre 12 a 120")
+            throw new InvalidUserDataException("La edad valida es solo entre 12 a 120");
         }
 
         // checar si el telefono es de exactamente 10 digitos
         if (request.phone().length() != 10 || !request.phone().matches("[0-9]+")){
-            throw new InvalidUserDataException("El telefono no es valido")
+            throw new InvalidUserDataException("El telefono no es valido");
         }
         if (!emailValidatorService.isValid(request.email())){
-            throw new InvalidUserDataException("El correo ingresado no es aceptado")
+            throw new InvalidUserDataException("El correo ingresado no es aceptado");
         }
-        if (userRepository.existsByUsername()){
-            throw new DuplicateUserNameException("El nombre de usuario ya existe")
+        if (userRepository.existsByUsername(request.username())){
+            throw new DuplicateUserNameException("El nombre de usuario ya existe");
         }
 
         var newUser = userRepository.save(
                 new User(request.username(), request.firstName(), request.lastName(), request.phone(), request.email(), request.age())
-        )
+        );
 
         return mapToResponse(newUser);
 
@@ -84,7 +86,7 @@ public class UserService {
         // TODO: buscar por id con findById, lanzar UserNotFoundException si está vacío, mapear y regresar
 
         if (id == null || id <= 0){
-            throw new InvalidUserDataException("El ID del usuario debe ser un numero positivo")
+            throw new InvalidUserDataException("El ID del usuario debe ser un numero positivo");
         }
 
         var userFromDB = userRepository.findById(id);
@@ -106,14 +108,14 @@ public class UserService {
         log.info("Suspendiendo usuario, id={}", id);
         // TODO: buscar usuario, validar status, cambiar a SUSPENDED, guardar, mapear y regresar
 
-        var userFromDB = userRepository.findById(id)
+        var userFromDB = userRepository.findById(id);
 
         if (userFromDB.isEmpty()) {
             throw new UserNotFoundException("User con id " + id + " no se ha encontrado");
         }
 
         var user = userFromDB.get();
-        user.setStatus(true);
+        user.setStatus(UserStatus.SUSPENDED);
         var saved = userRepository.save(user);
 
         log.info("User marcado como suspendido, id = {}", saved.getId());
@@ -131,9 +133,9 @@ public class UserService {
                 user.getFirstName(),
                 user.getLastName(),
                 user.getPhone(),
-                user.getEmail,
+                user.getEmail(),
                 user.getAge(),
                 user.getStatus()
-        )
+        );
     }
 }
